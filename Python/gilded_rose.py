8090d3ee1257eae70028a018d23c2a24619789e7
Nicolas Sauzede <nicolas.sauzede@gmail.com>
2024-11-01 23:10:26 +0100

    Extract brie_tick

--- Python/gilded_rose.py
+++ Python/gilded_rose.py
@@ -6,8 +6,11 @@ class GildedRose:
         self.quality = quality
 
     def tick(self):
-        if self.name == "Normal Item":
-            return self.normal_tick()
+        match self.name:
+            case "Normal Item":
+                return self.normal_tick()
+            case "Aged Brie":
+                return self.brie_tick()
         if self.name != "Aged Brie" and self.name != "Backstage passes to a TAFKAL80ETC concert":
             if self.quality > 0:
                 if self.name != "Sulfuras, Hand of Ragnaros":
@@ -35,9 +38,15 @@ class GildedRose:
             else:
                 if self.quality < 50:
                     self.quality = self.quality + 1
-
+    def brie_tick(self):
+        self.days_remaining -= 1
+        self.quality += 1
+        if self.days_remaining <= 0:
+            self.quality += 1
+        if self.quality > 50:
+            self.quality = 50
     def normal_tick(self):
-        self.days_remaining -=1
+        self.days_remaining -= 1
         if self.quality == 0:
             return
         self.quality -= 1
