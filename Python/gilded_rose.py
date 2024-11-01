87cca70ca6a0a24961e03abe97e3d54c91ec7f05
Nicolas Sauzede <nicolas.sauzede@gmail.com>
2024-11-01 22:49:09 +0100

    Refactor

--- Python/gilded_rose.py
+++ Python/gilded_rose.py
@@ -37,9 +37,9 @@ class GildedRose:
                     self.quality = self.quality + 1
 
     def normal_tick(self):
-        if self.quality > 0:
-            if self.days_remaining > 0:
-                self.quality -=1
-            if self.days_remaining <= 0:
-                self.quality -= 2
-        self.days_remaining -= 1
+        self.days_remaining -=1
+        if self.quality == 0:
+            return
+        self.quality -= 1
+        if self.days_remaining <= 0:
+            self.quality -= 1
