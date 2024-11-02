b4ab88c61847d747ff4ab97f847a815bcab6be27
Nicolas Sauzede <nicolas.sauzede@gmail.com>
2024-11-02 22:35:29 +0100

    Refactor into small objects

--- Python/gilded_rose.py
+++ Python/gilded_rose.py
@@ -1,21 +1,26 @@
-class GildedRose:
-
-    def __init__(self, name, days_remaining, quality):
-        self.name = name
-        self.days_remaining = days_remaining
-        self.quality = quality
-
+from dataclasses import dataclass
+@dataclass
+class Item:
+    quality:int; days_remaining:int
+    def tick(self):pass
+class Normal(Item):
     def tick(self):
-        match self.name:
-            case "Normal Item":
-                self.normal_tick()
-            case "Aged Brie":
-                self.brie_tick()
-            case "Sulfuras, Hand of Ragnaros":
-                self.sulfuras_tick()
-            case "Backstage passes to a TAFKAL80ETC concert":
-                self.backstage_tick()
-    def backstage_tick(self):
+        self.days_remaining -= 1
+        if self.quality == 0:
+            return
+        self.quality -= 1
+        if self.days_remaining <= 0:
+            self.quality -= 1
+class Brie(Item):
+    def tick(self):
+        self.days_remaining -= 1
+        self.quality += 1
+        if self.days_remaining <= 0:
+            self.quality += 1
+        if self.quality > 50:
+            self.quality = 50
+class Backstage(Item):
+    def tick(self):
         self.days_remaining -= 1
         if self.quality >= 50:
             return
@@ -27,19 +32,8 @@ class GildedRose:
             self.quality += 1
         if self.days_remaining < 5:
             self.quality += 1
-    def sulfuras_tick(self):
-        return # meh
-    def brie_tick(self):
-        self.days_remaining -= 1
-        self.quality += 1
-        if self.days_remaining <= 0:
-            self.quality += 1
-        if self.quality > 50:
-            self.quality = 50
-    def normal_tick(self):
-        self.days_remaining -= 1
-        if self.quality == 0:
-            return
-        self.quality -= 1
-        if self.days_remaining <= 0:
-            self.quality -= 1
+def GildedRose(name, days_remaining, quality):
+    return Normal(quality, days_remaining) if name == "Normal Item" else \
+        Brie(quality, days_remaining) if name == "Aged Brie" else \
+        Backstage(quality, days_remaining) if name == "Backstage passes to a TAFKAL80ETC concert" else \
+        Item(quality, days_remaining)
